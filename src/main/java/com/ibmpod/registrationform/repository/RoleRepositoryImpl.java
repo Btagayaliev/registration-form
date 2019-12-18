acb2e7b98fbd59e9e9e8b4627688617dcfedbbab
bakhtovar.tagayaliev <bakhtovar.tagayaliev@accenture.com>
2019-12-18 13:51:37 +0100

    update the role repository implementation

--- src/main/java/com/ibmpod/registrationform/repository/RoleRepositoryImpl.java
+++ src/main/java/com/ibmpod/registrationform/repository/RoleRepositoryImpl.java
@@ -1,5 +1,37 @@
 package com.ibmpod.registrationform.repository;
 
-public class RoleRepositoryImpl {
+import javax.persistence.EntityManager;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ibmpod.registrationform.model.Role;
+
+@Repository
+public class RoleRepositoryImpl implements RoleRepository {
+
+	@Autowired
+	private EntityManager entityManager;
+	
+	
+	public Role findRoleByName(String roleName) {
+		
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		Query<Role> query = currentSession.createQuery("from Role where name=:roleName", Role.class);
+		query.setParameter("roleName", roleName);
+		Role role = null;
+		
+		try {
+			role = query.getSingleResult();
+		} catch (Exception e) {
+			role = null;
+		}
+		
+		return role;
+	}
+
 
 }
